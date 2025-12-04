836df580b0975f31514f5e7c1d39ea1b74ace70e
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2025-12-05 04:08:42 +0800

    chore: upgrade to vite 8 beta
    Signed-off-by: Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>

--- vite.worker.config.ts
+++ vite.worker.config.ts
@@ -3,17 +3,26 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   build: {
+    cssCodeSplit: true,
+    cssMinify: true,
     emptyOutDir: false,
     minify: true,
+    modulePreload: {
+      polyfill: true,
+    },
     outDir: 'build',
     reportCompressedSize: true,
     rollupOptions: {
+      experimental: {
+        incrementalBuild: true,
+        nativeMagicString: true,
+        viteMode: true,
+      },
       input: './app/worker.ts',
       output: {
         entryFileNames: 'worker.js',
         format: 'esm',
       },
-      perf: true,
     },
     sourcemap: true,
     ssr: true,
