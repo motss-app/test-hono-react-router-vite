e30a41b89f2dbf27cdf4d517a9b48433b76917f5
The web walker <contact@motss.fyi>
2025-12-10 04:16:44 +0800

    chore: upgrade to vite 8 (#8)
    * chore: upgrade to vite 8 beta
    
    * feat: add canary deployment and custom vite plugin
    
    ---------
    
    Signed-off-by: Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>

--- vite.react-router.config.ts
+++ vite.react-router.config.ts
@@ -3,10 +3,17 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
+import { headersCopyPlugin } from './vite-plugins/copy-headers.ts';
+
+export default defineConfig(({ mode }) => ({
   plugins: [
     reactRouter(),
     tsconfigPaths(),
     tailwindcss(),
+    headersCopyPlugin({
+      dest: 'build/client/_headers',
+      headersDir: 'headers',
+      mode,
+    }),
   ],
-});
+}));
