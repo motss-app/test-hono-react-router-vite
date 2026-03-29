5cff8a6463b3d6874103b1771925459abc62812b
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2026-03-29 17:01:45 +0800

    chore: update dependencies, enable more RR flags
    Signed-off-by: Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>

--- react-router.config.ts
+++ react-router.config.ts
@@ -5,7 +5,10 @@ import { discoverPrerenderRoutes } from './app/utils/route-discovery.ts';
 export default {
   future: {
     unstable_optimizeDeps: true,
+    unstable_passThroughRequests: true,
+    unstable_previewServerPrerendering: true,
     unstable_subResourceIntegrity: true,
+    unstable_trailingSlashAwareDataRequests: true,
     v8_middleware: true,
     v8_splitRouteModules: true,
     v8_viteEnvironmentApi: true,
