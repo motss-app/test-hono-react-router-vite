8e54e8950486c955de54e6e8a9ee41deefdc509a
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2026-05-12 02:49:37 +0800

    Revert "feat: migrate gateway and BFF from @sentry/cloudflare withSentry to @sentry/hono middleware"
    This reverts commit 7827bc8412cc859d4b484d812659630cd205c584.

--- packages/bff/src/worker.ts
+++ packages/bff/src/worker.ts
@@ -1,18 +1,10 @@
-import { sentry } from '@sentry/hono/cloudflare';
 import { Hono } from 'hono';
 
 import { apiApp } from './api.ts';
 import type { BffBindings } from './bindings.ts';
-import { createCloudflareSentryOptions } from '../../../app/monitoring/sentry.ts';
 
 const app = new Hono<{
   Bindings: BffBindings;
-}>();
-
-app.use(sentry(app, (env: BffBindings) =>
-  createCloudflareSentryOptions(import.meta.env.MODE, env.SENTRY_DSN, import.meta.env.SENTRY_RELEASE)
-));
-
-app.route('/api', apiApp);
+}>().route('/api', apiApp);
 
 export default app;
